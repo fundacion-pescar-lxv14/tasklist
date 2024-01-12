ed3c0d4cd0e59dd300d6f62d1e93717edcfbbf81
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-12 17:33:37 -0300

    corregir rutas de aplicacion

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,6 +6,6 @@ export const getTasks = ({params:{_id}}, res) =>
 export const createTask = ({body}, res) => 
     Resolve(Task(body).save(), res)
 export const updateTask = ({params:{_id}, body}, res) => 
-    Resolve(Task.updateOne(_id,body), res)
+    Resolve(Task.updateOne({_id},body), res)
 export const deleteTask = ({params:{_id}}, res) => 
-    Resolve(Task.deleteOne(_id), res)
+    Resolve(Task.deleteOne({_id}), res)
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 import * as Task from "../controllers/tasks.js"
 const taskRouter = Router();
 
-taskRouter.post  ('/',      Task.createTask);
+taskRouter.post  ('/:_id?', Task.createTask);
 taskRouter.get   ('/:_id?', Task.getTasks  );
 taskRouter.put   ('/:_id?', Task.updateTask);
 taskRouter.delete('/:_id?', Task.deleteTask);
