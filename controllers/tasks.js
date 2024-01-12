a07b01b9749711024bf79e434273b55f4901953c
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-12 10:18:43 -0300

    implementar controladores de tareas

---
+++ controllers/tasks.js
@@ -0,0 +1,11 @@
+import { Task } from "../models/tasks.js";
+import { Resolve } from '../config/utils.js'
+
+export const getTasks = ({params:{_id}}, res) => 
+    Resolve(Task.find(_id?{_id}:{}), res)
+export const createTask = ({body}, res) => 
+    Resolve(Task(body).save(), res)
+export const updateTask = ({params:{_id}, body}, res) => 
+    Resolve(Task.updateOne(_id,body), res)
+export const deleteTask = ({params:{_id}}, res) => 
+    Resolve(Task.deleteOne(_id), res)
